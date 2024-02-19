8c2c9aa325abe8a4ef37263b75035a450b29c5a9
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2024-02-19 20:09:29 +0100

    Implement 'wc -l' command

--- unix-wc-command/ccwc/__init__.py
+++ unix-wc-command/ccwc/__init__.py
@@ -12,12 +12,29 @@ class ccwc():
                     filename = command[8:]
                     bytes = ccwc.read_bytes_size(f"./unix-wc-command/input/{filename}")
                     print(f"  {bytes} {filename}")
+                elif(first_part == "ccwc -l"):
+                    filename = command[8:]
+                    lines = ccwc.count_lines(f"./unix-wc-command/input/{filename}")
+                    print(f"  {lines} {filename}")
+                
 
     def read_bytes_size(file_name):
         try:
             with open(file_name, 'rb') as file:
                 # Read and print each line in the file
                 return len(file.read())
+        except FileNotFoundError:
+            print("File not found!")
+        except Exception as e:
+            print("An error occurred:", e)
+
+    def count_lines(file_name):
+        try:
+            with open(file_name, 'rb') as file:
+                line_count = 0
+                for line in file:
+                    line_count += 1
+            return line_count
         except FileNotFoundError:
             print("File not found!")
         except Exception as e:
